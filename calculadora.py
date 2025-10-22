0f18ef180222aff2d87bbb11dc2b63448f8844af
Miriam Martins <miriamgmartins1@gmail.com>
2025-10-22 20:59:05 -0300

    Adiciona função de subtração.

--- calculadora.py
+++ calculadora.py
@@ -2,9 +2,15 @@ def soma(a,b):
     soma = a+b
     return soma
 
+def subtracao(a,b):
+    subtracao= a-b
+    return subtracao
+
 a = float(input("Digite o núumero:"))
 b = float(input("Digite o núumero:"))
 
-resultado = soma(a,b)
-print(f"A soma é igual a: {resultado}")
+RSoma= soma(a,b)
+RSub= subtracao(a,b)
 
+print(f"A soma é igual a: {RSoma}")
+print(f"A subtraçção é igual a: {RSub}")
