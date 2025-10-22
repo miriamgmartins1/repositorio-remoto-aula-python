f94fa6e9288da8bdf1e19722e8af000f94b5b7c7
Miriam Martins <miriamgmartins1@gmail.com>
2025-10-22 20:54:04 -0300

    Adiciona função de soma.

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def soma(a,b):
+    soma = a+b
+    return soma
+
+a = float(input("Digite o núumero:"))
+b = float(input("Digite o núumero:"))
+
+resultado = soma(a,b)
+print(f"A soma é igual a: {resultado}")
+
